45d1c68db80055e59f567e47691e85812d2c971b
azizben01 <benazizsang1@gmail.com>
2024-11-01 09:33:05 +0200

    customer message

--- main.go
+++ main.go
@@ -229,10 +229,11 @@ func AdminLogin(context *gin.Context) {
 }
 
 type Message struct {
-	Name    string `json:"name"`
-	Email   string `json:"email"`
-	Message string `json:"message"`
-	Id 		int 	`json:"is"`
+	Name    		string `json:"name"`
+	Email   		string `json:"email"`
+	Message 		string `json:"message"`
+	Created_at 		*time.Time `json:"created_at"`
+	Id 				int 	`json:"is"`
 }
 func submitContactForm(context *gin.Context) {
     var contact Message
@@ -265,7 +266,7 @@ func MessageList(context *gin.Context) {
 	}
 	for rows.Next() {
 		var list Message
-		err := rows.Scan(&list.Name, &list.Email, &list.Message, &list.Id)
+		err := rows.Scan(&list.Name, &list.Email, &list.Message, &list.Created_at, &list.Id)
 		if err != nil {
 			context.JSON(http.StatusInternalServerError, gin.H{"error": "error processing customer message list"})
 			fmt.Println(err)
